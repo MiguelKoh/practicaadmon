fbac6320614ff3863fea2ef7636422de9ba4af72
MiguelKoh <81209063+MiguelKoh@users.noreply.github.com>
2021-12-03 21:31:01 -0600

    Add files via upload

---
+++ Evidencias/Katalon/Prueba1.java
@@ -0,0 +1,94 @@
+package com.example.CasoWeb;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Prueba1 {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+	WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) driver;
+  }
+
+  
+  @Test
+  public void testUADYVirtual() throws Exception {
+    driver.get("https://es.uadyvirtual.uady.mx/");
+    
+    pause(3000);
+  	String textoEjecucion =driver.getTitle();
+    String esperado="UADY Virtual Educación Superior: Ingresar al sitio";
+    
+    assertThat(esperado,is(textoEjecucion));
+  
+  //Autor: Miguel Koh Avila 
+  }
+  
+  private void pause(long mils) {
+	  try {
+	  Thread.sleep(mils);
+	  }catch(Exception e) {
+	  e.printStackTrace();
+	  }
+	  }
+ 
+  
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
